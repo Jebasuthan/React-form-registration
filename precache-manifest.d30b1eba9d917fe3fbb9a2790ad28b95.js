21b48af0f786051e51de02643b13e23131c5e4f6
Jeba Suthan <itjebasuthan@gmail.com>
2022-12-30 08:45:07 -0600

    Updates

---
+++ precache-manifest.d30b1eba9d917fe3fbb9a2790ad28b95.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b867d75aaa6a0fb2a44c9c76b9ffcad",
+    "url": "/React-form-registration/index.html"
+  },
+  {
+    "revision": "b2ccbebe6101d887904d",
+    "url": "/React-form-registration/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "34c4f47c387ac6cdf4f7",
+    "url": "/React-form-registration/static/css/main.432d715c.chunk.css"
+  },
+  {
+    "revision": "b2ccbebe6101d887904d",
+    "url": "/React-form-registration/static/js/2.3aea4c78.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-form-registration/static/js/2.3aea4c78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34c4f47c387ac6cdf4f7",
+    "url": "/React-form-registration/static/js/main.76658dce.chunk.js"
+  },
+  {
+    "revision": "868e6d58232b709878e6",
+    "url": "/React-form-registration/static/js/runtime-main.0ac64f32.js"
+  }
+]);
